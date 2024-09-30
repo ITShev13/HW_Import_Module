827933d95fa8752447a088cf066de3672dc145d0
Valerii Shevelev <Shev13VN@mail.ru>
2024-09-30 14:56:11 +0300

    edit file dirty_main.py

--- dirty_main.py
+++ dirty_main.py
@@ -1,4 +1,10 @@
+# Вариант с импортом конкретных функций
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from application.file_excel.unloading_XL import unloading_empl
 
+
+# вариант с импортом через *
+from application.db.people import *
+from application.salary import *
+from application.file_excel.unloading_XL import *
